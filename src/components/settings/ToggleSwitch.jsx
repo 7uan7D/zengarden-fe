6f01c91fc024c6c88175ae71d636084ed935cf3e
Soul Breaker <SoulBreaker2k3@gmail.com>
2025-03-08 16:14:58 +0700

    fix: adjust toggleSwitch design

--- src/components/settings/ToggleSwitch.jsx
+++ src/components/settings/ToggleSwitch.jsx
@@ -4,10 +4,10 @@ const ToggleSwitch = ({ label, isOn, onToggle }) => {
             <span className='text-gray-300'>{label}</span>
             <button className={`relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none
             ${isOn ? 'bg-indigo-600' : 'bg-gray-600'}`}
-                onClick={onToggle}
+            onClick={onToggle}
             >
-                <span className={`inline-block size-4 transform transition-transform bg-white rounded-full 
-                ${isOn ? 'translate-x-6' : 'translate-x-1'}`}
+                <span className={`absolute inline-block size-4 transform transition-transform bg-white rounded-full
+                ${isOn ? 'translate-x-1' : '-translate-x-4'}`}
                 />
             </button>
         </div>
